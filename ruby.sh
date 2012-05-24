6b45c66f47385784fe383faf610d80fd4e44ec89
Yang-Hsing Lin <travis.lin@wildmindcorp.com>
2012-05-24 12:34:41 +0800

    modify install instruction.

--- ruby.sh
+++ ruby.sh
@@ -11,10 +11,11 @@ aptInstall build-essential;
 aptInstall zlib1g-dev;
 aptInstall libssl-dev;
 
-white "now intalling libreadline-dev..."
+white "now intalling libreadline5-dev..."
 if sudo apt-get install libreadline5-dev; then
   green "install libreadline5-dev ok."
 else
+  white "install libreadline5-dev failed, now try libreadline-gplv2-dev"
   aptInstall libreadline-gplv2-dev;
 fi
 
@@ -24,7 +25,7 @@ echo "[[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm" >> ~/.ba
 source ~/.bash_profile
 
 if rvm install 1.9.3; then
-  white 'execute "source ~/.profile to finish the installation."'
+  white 'execute "source ~/.bash_profile to finish the installation."'
   green "install ruby 1.9.3 ok.";
 else
   red "install ruby 1.9.3 failed."
