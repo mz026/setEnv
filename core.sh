a4ddffd1b9e70b3fd7c3025b81c745a29e53ceb8
Yang-Hsing Lin <travis.lin@wildmindcorp.com>
2012-05-25 19:44:11 +0800

    add phpunit and pear.

--- core.sh
+++ core.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source _tool.sh
 
+aptUpdate;
 aptInstall curl;
 aptInstall apache2;
 aptInstall mysql-client;
@@ -8,13 +9,28 @@ aptInstall mysql-server;
 aptInstall php5;
 aptInstall libapache2-mod-php5;
 aptInstall libapache2-mod-auth-mysql;
+aptInstall php5-xdebug;
 aptInstall php5-mysql;
 aptInstall php5-gd;
 aptInstall php5-cli;
 aptInstall php5-curl;
 aptInstall php5-tidy;
+aptInstall php-pear;
 aptInstall git-core;
 
+white "now discovering pear channels..."
+sudo pear upgrade pear
+sudo pear channel-discover pear.phpunit.de
+sudo pear channel-discover pear.symfony-project.com
+sudo pear channel-discover components.ez.no
+sudo pear update-channels
+sudo pear upgrade-all
+
+white "installing phpunit ..."
+sudo pear install --alldeps phpunit/PHPUnit
+
+white "installing phpunit_selenium ..."
+sudo pear install phpunit/phpunit_selenium
 
 green "ya!";
 
