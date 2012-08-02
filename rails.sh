139b4774dbca71479611423c19b2dddb0059e14c
mz026 <mz026@mz026-home.(none)>
2012-08-02 16:10:04 +0800

    skip apache2 if installed.

--- rails.sh
+++ rails.sh
@@ -20,7 +20,13 @@ gemInstall mysql;
 gemInstall passenger;
 
 aptInstall libcurl4-openssl-dev
-aptInstall apache2
+
+if [[ "`which apache2ctl`" == '' ]]; then
+  aptInstall apache2
+else
+  white "apache2 has been installed, skip it."
+fi
+
 aptInstall apache2-prefork-dev
 
 passenger-install-apache2-module
