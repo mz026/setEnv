a3454e416986908f6599d2a11c75043fee0666f9
Yang-Hsing Lin <yanghsing.lin@thekono.com>
2013-07-11 17:32:11 +0800

    remove bash gitconfig and vimrc

--- bash.sh
+++
@@ -1,3 +0,0 @@
-git clone git://github.com/mz026/bashrc.git ~/.bashrcRepo
-
-echo 'source ~/.bashrcRepo/bashrc.bash' >> ~/.bashrc
--- gitconfig.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source _tool.sh
-
-white "now cloning gitconfig repository...";
-git clone git://github.com/mz026/gitconfig.git ~/.gitconfigRepo;
-green "cloned gitconfig to .gitconfigRepo !";
-
-ln -s ~/.gitconfigRepo/.gitconfig ~/.gitconfig
-green "made a symbolic link ~/.gitconfig to ~/.gitconfigRepo/.gitconfig";
